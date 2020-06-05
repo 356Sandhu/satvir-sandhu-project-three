992a4334a72dbd8245e259c46ed55b17723145a9
Satvir Sandhu <356Sandhu@gmail.com>
2020-06-05 15:30:05 -0400

    store form values, and perform basic calcs

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   console.log("ready!");
   $("#form-stuff").submit(function (e) {
     e.preventDefault();
-    console.log($("#target-revenue").val());
+    let target_revenue = $("#target-revenue").val();
+    let subscription = $("#subscription").val();
+    let time_frame = $("#time-frame").val();
+    console.log(target_revenue, subscription, time_frame);
   });
 });
