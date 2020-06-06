90d793ad6d5c78e364950904c2432ec6cb26b2ae
Satvir Sandhu <356Sandhu@gmail.com>
2020-06-06 12:37:56 -0400

    make form semantic and calculate breakdown in js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ $(document).ready(function () {
     let target_revenue = $("#target-revenue").val();
     let subscription = $("#subscription").val();
     let time_frame = $("#time-frame").val();
+    let breakdown = [];
+    for (let i = 0; i < 6; i++) {
+      breakdown[i] = target_revenue * ((i + 1) / 6);
+    }
     console.log(target_revenue, subscription, time_frame);
+    console.table(breakdown);
   });
 });
